ca43654c5c2840c29ed09a23782fa7d125e7810e
Siegfried Albrecht <mail@albrecht-s.de>
2024-05-06 10:48:58 +0200

    deploy to custom domain

--- svelte.config.js
+++ svelte.config.js
@@ -21,8 +21,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({ fallback: '404.html' }),
-		paths: { base: process.env.BASE_PATH !== '' ? process.env.BASE_PATH : '',}
+		adapter: adapter({ fallback: '404.html' })}
 	}
 };
 export const trailingSlash = 'always';
